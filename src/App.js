3b3673659a38262dc313bca725c1c909d59bd727
Rajiv Kumar Gupta <100814533+rajivgupta1@users.noreply.github.com>
2023-07-31 15:35:03 +1000

    flexbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Forms from './Components/Forms';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+        <h1>Hello Rambo Come Flex Box</h1>
+
+    <div className='parent'>
+    <div className='item-1'>1</div>
+    <div className='item-2'>2</div>
+    <div className='item-3'>3</div>
+    <div className='item-4'>4</div>
+    <div className='item-5'>5</div>
+    <div className='item-6'>6</div>
+    <div className='item-7'>7</div>
+    <div className='item-8'>8</div>
+    <div className='item-9'>9</div>
+
+    
+
     </div>
-  );
+      {/* <Forms/> */}
+    </>
+  )
 }
 
-export default App;
+export default App
